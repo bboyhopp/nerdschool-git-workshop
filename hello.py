9be6a2788e0c2c8df1f7494e3b8321af471f6060
Balder <balderhaugstvedt@gmail.com>
2025-04-01 19:47:14 +0200

    Added last name to hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
-name = input("What is your name? ")
+first = input("What is your first name? ")
+last = input("What is your last name? ")
 
-print(f"Hello, {name}!")
+print(f"Hello, {first} {last}!")
